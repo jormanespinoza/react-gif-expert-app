a00f7e1b2dd4b810a337e566756b4f5644fdedbc
Jorman <espinoza.dev@gmail.com>
2021-01-10 16:29:42 -0300

    Renaming repo

---
+++ src/GifExpertApp.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { AddCategory } from './Components/AddCategory';
+import { GifGrid } from './Components/GifGrid';
+
+export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['Dragon Ball']);
+
+    return (
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+
+            {
+                categories.map(category => (
+                    <GifGrid
+                        key={category}
+                        category={category}
+                    />
+                ))
+            }
+        </>
+    )
+}
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,18 @@
+export const getGifs = async (category) => {
+    const limit = 10;
+    const apiKey = 'a6gn2SGDDwcxAX3N4XEWhwClj4Vx6s9h';
+    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=${limit}&api_key=${apiKey}`;
+
+    const response = await fetch(url);
+    const { data } = await response.json();
+
+    const gifs = data.map(img => (
+        {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    ));
+
+    return gifs;
+}
