7f1878a51a526e66d1a58c4448f544bfffee675a
Jorman <espinoza.dev@gmail.com>
2021-01-10 16:40:56 -0300

    Rebuilding project docs

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,7 +1,7 @@
 export const getGifs = async (category) => {
     const limit = 10;
     const apiKey = 'a6gn2SGDDwcxAX3N4XEWhwClj4Vx6s9h';
-    const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=${limit}&api_key=${apiKey}`;
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=${limit}&api_key=${apiKey}`;
 
     const response = await fetch(url);
     const { data } = await response.json();
