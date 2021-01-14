a6208c8c2191470d30650e8ef526e551c6e89238
Jorman <espinoza.dev@gmail.com>
2021-01-14 09:53:26 -0300

    Simulating submits and changes on AddCategory component

--- src/tests/component/AddCategory.test.js
+++ src/tests/component/AddCategory.test.js
@@ -4,8 +4,13 @@ import { AddCategory } from '../../Components/AddCategory';
 
 describe('Tests on AddCategory component', () => {
 
-    const setCategories = () => {};
-    const wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    });
 
     test('should render <AddCategory /> properly', () => {
         expect(wrapper).toMatchSnapshot();
@@ -14,7 +19,26 @@ describe('Tests on AddCategory component', () => {
     test('should change on input change', () => {
         const input = wrapper.find('input');
         const value = 'Hola Mundo'
-        input.simulate('change', {target: {value}});
+        input.simulate('change', { target: { value } });
+
         expect(wrapper.find('.hidden').text().trim()).toBe(value);
+    });
+
+    test('should post anything on submit when given input is not valid', () => {
+        wrapper.find('form').simulate('submit', { preventDefault() { } });
+
+        expect(setCategories).not.toHaveBeenCalled();
+    });
+
+    test('should call setCategories and empty inputValue', () => {
+        const value = 'Hola Mundo'
+        const input = wrapper.find('input');
+        input.simulate('change', { target: { value } });
+        wrapper.find('form').simulate('submit', { preventDefault() {} });
+
+        expect(setCategories).toHaveBeenCalled();
+        expect(setCategories).toHaveBeenCalledTimes(1);
+        expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+        expect(input.prop('value')).toBe('')
     });
 });
